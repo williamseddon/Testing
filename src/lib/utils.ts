a5472ef07fb0da96a93e293de898ea2ccf1ba0ab
williamseddon <66979736+williamseddon@users.noreply.github.com>
2026-02-03 13:01:39 -0500

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx'
-
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs)
-}
-
-export function clamp(n: number, min: number, max: number) {
-  return Math.max(min, Math.min(max, n))
-}
